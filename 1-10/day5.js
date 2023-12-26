76c26b5af26635db2dd826ac555e25f95ec88e00
Alexlegard <alexlegard3@gmail.com>
2023-12-26 17:21:40 -0500

    Day5.js
    Day5.js

---
+++ 1-10/day5.js
@@ -0,0 +1,9 @@
+// Today I was working on dark mode for my personal website, alexlegard.ca.
+// It is a work in progress though. I'll have something to show for it soon.
+
+// I was getting my bearings for how the code for my website is structured
+// since I haven't worked on it for a while.
+
+// I had to figure out why changing certain colors wasn't possible using
+// css files. Turns out I'm using Bootstrap, so I had to use certain class
+// names like bg-light and bg-dark.
