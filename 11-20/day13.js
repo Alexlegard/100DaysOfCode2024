e63a7875634d3f010e1986e73a011f082bb96160
Alexlegard <alexlegard3@gmail.com>
2024-01-04 00:12:26 -0500

    day13.js
    Day 13: React auth Youtube session

---
+++ 11-20/day13.js
@@ -0,0 +1,16 @@
+// Today, I mostly explored various articles and videos about React
+// authentication. My React app had a branch with some code for
+// auth0, but I really wasn't happy that I couldn't find a tutorial
+// on auth0 that I was happy with.
+
+// I changed my branch from "auth0" to master. Then I made sure I
+// could run my React app and I booted up my laptop and did the
+// exact same thing so I can run it fine on my laptop as well. Super
+// important to make sure things were working before going to the
+// next Coffee 'n' Code meetup.
+
+// I found this tutorial series that looks promising by Dave Gray:
+// https://www.youtube.com/watch?v=brcHK3P6ChQ&list=PL0Zuz27SZ-6PRCpm9clX0WiBEMB70FWwd&index=2
+// The first video is about creating a registration form.
+
+// I'll also mention I made a new resume this morning. Cool!
