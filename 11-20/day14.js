7ad2ecafcb2904b96555a715498342934ef9a86a
Alexlegard <alexlegard3@gmail.com>
2024-01-05 00:20:35 -0500

    Day 14
    alexlegard.ca dark mode button, React register page.

---
+++ 11-20/day14.js
@@ -0,0 +1,13 @@
+// I started the day by making a couple of css changes to my website.
+// -Dark mode button turns white while in dark mode.
+// -Dark mode button has padding on top.
+
+// In the React Recipes, I deleted the Indiana Drag Scroll package just
+// to clean things up a little bit.
+
+// I began Dave Gray's long tutorial for React auth using Axios:
+// https://www.youtube.com/watch?v=brcHK3P6ChQ&list=PL0Zuz27SZ-6PRCpm9clX0WiBEMB70FWwd&index=2
+// I'm currently 26 minutes into the video. I transcribed his register
+// component, made a new route for it so I can visit the Register page
+// by typing /register in the url. Made a few bug fixes so my React
+// app doesn't immediately crash...
