298258b51d4dceb9695ef1133dc7cd36a232398e
Alexlegard <alexlegard3@gmail.com>
2024-01-05 23:48:39 -0500

    day15.js
    Day 15: Register CSS

---
+++ 11-20/day15.js
@@ -0,0 +1,4 @@
+// Today I mostly wrote the register form css in my React recipes app.
+// I also had to write some JSX in my component to smooth out the logic
+// for displaying or hiding those tooltips that tell you what makes a
+// valid username or password.
