44eb76ff8d7dd649f9438a337af57c62be65ff80
Alexlegard <alexlegard3@gmail.com>
2024-01-19 23:42:45 -0500

    Day 27
    Python basics & environment setup

---
+++ 21-30/day27.py
@@ -0,0 +1,13 @@
+msg = "Hello Python World!"
+print(msg)
+msg = "Hello Python Crash Course World!"
+print(msg)
+name = "alex legard"
+print(name.title()) # Title case: Alex Legard
+print(name.upper()) # Uppercase: ALEX LEGARD
+print(name.lower()) # Lowercase: alex legard
+first_name = "Alex"
+last_name = "Legard"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name}")
