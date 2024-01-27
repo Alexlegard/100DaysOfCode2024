9cca547eca6397f2d0eb3092601943aa2c4c5b6a
Alexlegard <alexlegard3@gmail.com>
2024-01-27 01:47:38 -0500

    Day 32
    Reactjs chess video

---
+++ 31-40/day32.js
@@ -0,0 +1,7 @@
+// Today I was just watching a video on making a Reactjs chess app:
+
+// https://www.youtube.com/watch?v=Iri__zwxwHg&list=PLBmRxydnERkysOgOS917Ojc_-uisgb8Aj
+
+// I was thinking about if this is something I want to get started with.
+// It seems like quite a long Youtube series and it'll be even more
+// challenging if I decide to make a chess variant instead.
