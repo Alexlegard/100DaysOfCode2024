8aad0ef790ebf473737041fc5ef0727a479a7c8c
Alexlegard <alexlegard3@gmail.com>
2024-03-29 15:08:46 -0400

    Day 44 React tests

---
+++ 41-50/day44.js
@@ -0,0 +1,5 @@
+// Today I put the new class and test into my React project folder and
+// everything's been working out great. Refactored my Game.jsx so that
+// it uses the Chessboard class. I also renamed the class to
+// ChessboardClass so it doesn't use the same name as the Chessboard
+// React component.
